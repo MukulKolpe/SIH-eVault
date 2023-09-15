5b81811abd9fcae914854d2bc9676281ec4b0620
Mukul Kolpe <mukulkolpe45@gmail.com>
2023-09-15 07:24:08 +0530

    Document upload UI

---
+++ src/pages/upload-document/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UploadDocumentForm from "../../components/UploadDocumentForm/Form";
+
+const UploadDocument = () => {
+  return (
+    <div>
+      <UploadDocumentForm />
+    </div>
+  );
+};
+
+export default UploadDocument;
